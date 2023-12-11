508c320e8b9d6a5eff8b1744cbee0f60da897757
shimisnow <14943507+shimisnow@users.noreply.github.com>
2023-12-11 08:29:02 -0300

    test: AreaWithRadius.setProperties()

---
+++ src/generator/area-with-radius.spec.ts
@@ -0,0 +1,68 @@
+import {Polygon} from '../enums/polygon.enum';
+import {GeoPoint} from '../types/geo-point.type';
+import {AreaWithRadius} from './area-with-radius';
+
+class AreaWithRadius_Dummy extends AreaWithRadius {
+  public constructor(
+    centerCoordinate: GeoPoint,
+    radiusInMeters: number,
+    type: Polygon
+  ) {
+    super(centerCoordinate, radiusInMeters, type);
+  }
+}
+
+describe('class AreaWithRadius', () => {
+  describe('setProperties()', () => {
+    test('method unused', () => {
+      const obj = new AreaWithRadius_Dummy(
+        {latitude: -22.952125, longitude: -43.210516},
+        1000,
+        Polygon.CIRCLE
+      );
+
+      const properties = obj.getProperties();
+
+      expect(properties).toBeUndefined();
+    });
+
+    test('method used with merge enabled', () => {
+      const obj = new AreaWithRadius_Dummy(
+        {latitude: -22.952125, longitude: -43.210516},
+        1000,
+        Polygon.CIRCLE
+      );
+
+      obj.setProperties({propA: 'A'});
+      obj.setProperties({propB: 'B'});
+      obj.setProperties({propC: 'D'});
+      obj.setProperties({propC: 'C'});
+
+      const properties = obj.getProperties();
+
+      expect(properties).toHaveProperty('propA');
+      expect(properties.propA).toBe('A');
+      expect(properties).toHaveProperty('propB');
+      expect(properties.propB).toBe('B');
+      expect(properties).toHaveProperty('propC');
+      expect(properties.propC).toBe('C');
+    });
+
+    test('method used with merge disabled', () => {
+      const obj = new AreaWithRadius_Dummy(
+        {latitude: -22.952125, longitude: -43.210516},
+        1000,
+        Polygon.CIRCLE
+      );
+
+      obj.setProperties({propA: 'A'}, true);
+      obj.setProperties({propB: 'B'}, true);
+
+      const properties = obj.getProperties();
+
+      expect(properties).not.toHaveProperty('propA');
+      expect(properties).toHaveProperty('propB');
+      expect(properties.propB).toBe('B');
+    });
+  });
+});
